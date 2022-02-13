24263ee1707295e567be476a01769ca469d50062
Jacqueline Teixeira <teixeijf@gmai.com>
2022-02-13 18:14:52 -0300

    feat(login): validação formulário

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { MensagemModule } from './../components/mensagem/mensagem.module';
+
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
-  imports: [CommonModule, HomeRoutingModule, FormsModule],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
